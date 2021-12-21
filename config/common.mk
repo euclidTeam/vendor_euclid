bb3458995c06d860a84cb6a095cc77f633c7effe
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    vendor: Bring back UDFPS icons and animations [3/3]
    Signed-off-by: Anush02198 <anush.4376@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -111,6 +111,12 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# UDFPS Animations
+ifeq ($(EXTRA_UDFPS_ANIMATIONS),true)
+PRODUCT_PACKAGES += \
+    UdfpsResources
+endif
+
 ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(EUCLID_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
