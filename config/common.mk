44b8ea5aba30cabe0c13e76c4023c3e994b3f4d2
nift4 <nift4@protonmail.com>
2023-04-14 00:37:39 +0200

    Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf

--- config/common.mk
+++ config/common.mk
@@ -41,11 +41,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/euclid/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/euclid/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/euclid/prebuilt/common/bin/50-euclid.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-euclid.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-euclid.sh
+    vendor/euclid/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions 
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
