0c940a2ad5c5b267f0d636d623a87b3e97711bc2
NRanjan-17 <nalinishranjan05@gmail.com>
2024-01-20 01:17:23 +0530

    Nuke backuptools
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -32,30 +32,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
-# Backup Tool
-PRODUCT_COPY_FILES += \
-    vendor/euclid/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/euclid/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/euclid/prebuilt/common/bin/50-euclid.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-euclid.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-euclid.sh
-
-ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
-PRODUCT_COPY_FILES += \
-    vendor/euclid/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
-    vendor/euclid/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
-    vendor/euclid/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/bin/backuptool_ab.sh \
-    system/bin/backuptool_ab.functions \
-    system/bin/backuptool_postinstall.sh
-
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
-endif
 endif
 
 # DroidX-specific init rc file
