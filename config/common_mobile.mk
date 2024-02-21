63051d2258e827f1cf513467e918c23d6f83d772
Michael Bestas <mkbestas@lineageos.org>
2024-02-21 16:40:56 +0200

    config: Include AudioPackage14.mk
    Change-Id: I3c02322ec5d4cb2242193dbd3b120a8a1842a63a
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -2,8 +2,14 @@
 $(call inherit-product, vendor/euclid/config/common.mk)
 
 # Include AOSP audio files
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 include vendor/euclid/config/aosp_audio.mk
 
+
+# Default notification/alarm sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg
 
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
