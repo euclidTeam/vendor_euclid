f18bc8cc2e085ed0802865d6e50e57111e510381
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-05-19 16:12:29 +0530

    Added Few More Fonts
    Signed-off-by: ZirgomHaidar <zirgomhaidar17@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -24,6 +24,54 @@ PRODUCT_PACKAGES += \
     GInterVF-Italic.ttf \
     GInterVF-Roman.ttf \
     Manrope-VF.ttf \
+    
+# Author
+PRODUCT_PACKAGES += \
+    Author-Variable.ttf \
+    Author-VariableItalic.ttf
+
+# Cabinet-Grotesk
+PRODUCT_PACKAGES += \
+    CabinetGrotesk-Variable.ttf
+
+
+# Eudoxus-Sans
+PRODUCT_PACKAGES += \
+    EudoxusSansGX.ttf
+
+# Fixel
+PRODUCT_PACKAGES += \
+    FixelVariable.ttf \
+    FixelVariableItalic.ttf
+
+# Satoshi
+PRODUCT_PACKAGES += \
+    Satoshi-Variable.ttf \
+    Satoshi-VariableItalic.ttf
+
+# Space-Grotesk
+PRODUCT_PACKAGES += \
+    SpaceGrotesk-Variable.ttf
+
+# Switzer
+PRODUCT_PACKAGES += \
+    Switzer-Variable.ttf \
+    Switzer-VariableItalic.ttf
+
+# Uncut-Sans
+PRODUCT_PACKAGES += \
+    Uncut-Sans-VF.ttf
+
+# DM-Sans
+PRODUCT_PACKAGES += \
+    dm-sans-latin-400-italic.ttf \
+    dm-sans-latin-400-normal.ttf \
+    dm-sans-latin-500-italic.ttf \
+    dm-sans-latin-500-normal.ttf \
+    dm-sans-latin-600-italic.ttf \
+    dm-sans-latin-600-normal.ttf \
+    dm-sans-latin-700-italic.ttf \
+    dm-sans-latin-700-normal.ttf
 
 # Register vendor fonts
 PRODUCT_COPY_FILES += \
