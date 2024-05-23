Evolution-X-AOSP/vendor_evolution

envestup: Always use `python3` when executing `repopick`
* Otherwise we get "permission denied" as the file doesn't have the
  executable bit set.

Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

build/envsetup.sh
@@ -600,7 +600,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/vendor/evolution/build/tools/repopick.py $@
+    python3 $T/vendor/evolution/build/tools/repopick.py $@
 }
 
 function sort-blobs-list() {

euclidTeam/vendor_euclid

build/envsetup.sh
@@ -951,7 +951,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/vendor/euclid/build/tools/repopick.py $@
+    python3 $T/vendor/euclid/build/tools/repopick.py $@
 }
 
 function sort-blobs-list() {