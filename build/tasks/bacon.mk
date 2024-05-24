07f5b18504c2c418adee0947fcba53ca0f3999bb
PainKiller3 <ninadpatil100@gmail.com>
2024-05-24 21:54:57 +0530

    vendor: tools: Add json generating script
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -25,10 +25,11 @@ euclid: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ./vendor/euclid/build/tasks/ascii_output.sh
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(EUCLID_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(EUCLID_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(EUCLID_TARGET_PACKAGE).sha256sum
+	$(hide) ./vendor/euclid/build/tools/generate_json_build_info.sh $(EUCLID_TARGET_PACKAGE)
 	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
-	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(EUCLID_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(EUCLID_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(EUCLID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.euclid.build.date | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(EUCLID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Timestamp:"${CL_PRP}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_PRP}" `du -sh $(EUCLID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filehash: "${CL_PRP}" `md5sum $(EUCLID_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filename: "${CL_PRP} $(EUCLID_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"ID: "${CL_PRP}" `cat $(EUCLID_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
---
+++ build/tools/generate_json_build_info.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+CL_RED="\033[31m"
+CL_CYN="\033[1;36m"
+CL_PRP="\033[35m"
+CL_NC="\033[0m"
+if [ "$1" ]; then
+    echo "Generating .json"
+    file_path=$1
+    file_name=$(basename "$file_path")
+    DEVICE=$(echo $TARGET_PRODUCT | sed 's/euclid_//g')
+    if [ -f $file_path ]; then
+        # only generate for official builds. unless forced with 'export FORCE_JSON=1'
+        if [[ $file_name == *"official"* ]] || [[ $FORCE_JSON == 1 ]]; then
+            if [[ $FORCE_JSON == 1 ]]; then
+                echo -e "${CL_CYN}Forced generation of json${CL_NC}"
+            fi
+            file_size=$(stat -c%s $file_path)
+            md5=$(md5sum $file_path | awk '{ print $1 }');
+            timestamp=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            id=$(cat "$file_path.sha256sum" | cut -d' ' -f1);
+            build_type=$(grep ro\.euclid\.buildtype ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            base_version=$(grep ro\.euclid\.base\.version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            link="https://sourceforge.net/projects/euclidOS/files/${DEVICE}/${file_name}/download"
+            echo "{" > $file_path.json
+            echo "  \"response\": [" >> $file_path.json
+            echo "    {" >> $file_path.json
+            echo "      \"timestamp\": ${timestamp}," >> $file_path.json
+            echo "      \"size\": ${file_size}," >> $file_path.json
+            echo "      \"filehash\": \"${md5}\"," >> $file_path.json
+            echo "      \"filename\": \"${file_name}\"," >> $file_path.json
+            echo "      \"id\": \"${id}\"," >> $file_path.json
+            echo "      \"romtype\": \"${build_type}\"," >> $file_path.json
+            echo "      \"version\": \"${base_version}\"," >> $file_path.json
+            echo "      \"url\": \"${link}\"" >> $file_path.json
+            echo "    }" >> $file_path.json
+            echo "  ]" >> $file_path.json
+            echo "}" >> $file_path.json
+            mv "${file_path}.json" "./${DEVICE}.json"
+            echo -e "${CL_CYN}Done generating ${CL_PRP}${DEVICE}.json${CL_NC}"
+        else
+            echo -e "${CL_RED}Skipped generating json for a non-official build${CL_NC}"
+        fi
+    fi
+fi
