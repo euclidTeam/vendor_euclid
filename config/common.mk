drishal/vendor_droidx

vendor: init: Implement a better way to hide magisk
Some banking apps read the file /proc/net/unix to
find out whether things like Magisk are installed/running.

To prevent that, chmod it 440.
This file isn't needed by any other process when boot is finished.

I've tested this and banking apps that did not work before
and recognized Magisk being installed now don't recognize
that and work just fine.

Change-Id: Icd2be51436fa13cc52748e6d03937d0cb2e713f1
Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
Signed-off-by: hipexscape <abhinav.115260@gmail.com>

config/common.mk
@@ -67,6 +67,9 @@ PRODUCT_COPY_FILES += \
     vendor/droidx/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
     vendor/droidx/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
 
+PRODUCT_COPY_FILES += \
+    vendor/droidx/prebuilt/common/etc/init/init.custom-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-system_ext.rc
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml

euclidTeam/vendor_euclid

Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

config/common.mk
@@ -61,6 +61,9 @@ PRODUCT_COPY_FILES += \
     vendor/euclid/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
     vendor/euclid/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
 
+PRODUCT_COPY_FILES += \
+    vendor/euclid/prebuilt/common/etc/init/init.custom-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-system_ext.rc
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml