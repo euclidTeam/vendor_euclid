DroidX-UI/vendor_droidx

common: Sign with our private keys if-exists

config/common.mk
@@ -262,3 +262,4 @@ include vendor/droidx/config/version.mk
 include vendor/droidx/config/bootanimation.mk
 include vendor/droidx/config/telephony.mk
 include vendor/droidx/config/themes.mk
+-include vendor/droidx-priv/keys/keys.mk

euclidTeam/vendor_euclid

config/common.mk
@@ -258,3 +258,4 @@ include vendor/euclid/config/version.mk
 include vendor/euclid/config/bootanimation.mk
 include vendor/euclid/config/telephony.mk
 include vendor/euclid/config/themes.mk
+-include vendor/euclid-priv/keys/keys.mk