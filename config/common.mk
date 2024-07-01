zaidkhan0997/vendor_blaze

Build Updater To Enable `OTA`
* We've made updater App For Official Device Only

Took Reference From : https://github.com/Project-PixelStar/vendor_pixelstar/commit/0dd2f54b0afd75bc98b464c4f133d1ec77de98d8

config/common.mk
@@ -299,5 +299,6 @@ PRODUCT_PACKAGES += \
     Aperture \
     BlazeWallpaperStub
 
+include vendor/blaze/config/ota.mk
 include vendor/blaze/config/pixel_props.mk
 include vendor/blaze/config/packages.mk

config/ota.mk
@@ -1,0 +1,9 @@
+# Only include Updater for official  build
+ifeq ($(filter-out OFFICIAL,$(BLAZE_BUILD_TYPE)),)
+    PRODUCT_PACKAGES += \
+        Updater
+
+PRODUCT_COPY_FILES += \
+    vendor/blaze/prebuilt/common/etc/init/init.blaze-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.blaze-updater.rc
+endif
+

euclidTeam/vendor_euclid

config/common.mk
@@ -284,13 +284,6 @@ PRODUCT_PACKAGES += \
     ThemePicker \
     ThemesStub
 
-# Updater
-PRODUCT_PACKAGES += \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/euclid/prebuilt/common/etc/init/init.euclid-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.euclid-updater.rc
-
 # Apps
 PRODUCT_PACKAGES += \
     Aperture \
@@ -318,6 +311,8 @@ endif
 # Fonts
 $(call inherit-product, vendor/euclid/config/fonts.mk)
 
+
+include vendor/euclid/config/ota.mk
 include vendor/euclid/config/version.mk
 include vendor/euclid/config/bootanimation.mk
 include vendor/euclid/config/telephony.mk

config/ota.mk
@@ -1,0 +1,9 @@
+# Only include Updater for official  build
+ifeq ($(filter-out OFFICIAL,$(EUCLID_BUILD_TYPE)),)
+    PRODUCT_PACKAGES += \
+    Updater
+
+PRODUCT_COPY_FILES += \
+    vendor/euclid/prebuilt/common/etc/init/init.euclid-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.euclid-updater.rc
+endif
+