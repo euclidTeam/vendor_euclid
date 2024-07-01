drishal/vendor_droidx

fonts: Initial import of fonts from PixelOs-AOSP
* Credit goes to Pixel OS

Signed-off-by: hipexscape <abhinav.115260gmail.com>
Signed-off-by: ZirgomHaidar <zirgomhaidar17@gmail.com>

config/common.mk
@@ -245,8 +245,10 @@ $(call inherit-product-if-exists, vendor/SystemUIClocks/product.mk)
 # Vendor Certification
 $(call inherit-product-if-exists, vendor/certification/config.mk)
 
+# Fonts
+$(call inherit-product, vendor/droidx/config/fonts.mk)
+
 include vendor/droidx/config/version.mk
 include vendor/droidx/config/bootanimation.mk
 include vendor/droidx/config/telephony.mk
-#include vendor/droidx/config/fonts.mk
 include vendor/droidx/config/themes.mk

config/fonts.mk
@@ -1,0 +1,31 @@
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf \
+    GoogleSansClock-Regular.ttf \
+    GoogleSansFlex-Regular.ttf
+
+# General Sans
+PRODUCT_PACKAGES += \
+    GeneralSans-Bold.ttf \
+    GeneralSans-BoldItalic.ttf \
+    GeneralSans-Italic.ttf \
+    GeneralSans-Medium.ttf \
+    GeneralSans-Regular.ttf \
+    GeneralSans-MediumItalic.ttf
+
+# Customization overlays
+PRODUCT_PACKAGES += \
+    FontGInterOverlay \
+    FontGoogleSansOverlay \
+    FontManropeOverlay \
+
+PRODUCT_PACKAGES += \
+    GInterVF-Italic.ttf \
+    GInterVF-Roman.ttf \
+    Manrope-VF.ttf \
+
+# Register vendor fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/droidx/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/droidx/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml

euclidTeam/vendor_euclid

config/common.mk
@@ -229,8 +229,10 @@ $(call inherit-product, vendor/bcr/bcr.mk)
 # Inherit SystemUI Clocks if they exist
 $(call inherit-product-if-exists, vendor/SystemUIClocks/product.mk)
 
+# Fonts
+$(call inherit-product, vendor/euclid/config/fonts.mk)
+
 include vendor/euclid/config/version.mk
 include vendor/euclid/config/bootanimation.mk
 include vendor/euclid/config/telephony.mk
-#include vendor/euclid/config/fonts.mk
 include vendor/euclid/config/themes.mk

config/fonts.mk
@@ -1,0 +1,31 @@
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf \
+    GoogleSansClock-Regular.ttf \
+    GoogleSansFlex-Regular.ttf
+
+# General Sans
+PRODUCT_PACKAGES += \
+    GeneralSans-Bold.ttf \
+    GeneralSans-BoldItalic.ttf \
+    GeneralSans-Italic.ttf \
+    GeneralSans-Medium.ttf \
+    GeneralSans-Regular.ttf \
+    GeneralSans-MediumItalic.ttf
+
+# Customization overlays
+PRODUCT_PACKAGES += \
+    FontGInterOverlay \
+    FontGoogleSansOverlay \
+    FontManropeOverlay \
+
+PRODUCT_PACKAGES += \
+    GInterVF-Italic.ttf \
+    GInterVF-Roman.ttf \
+    Manrope-VF.ttf \
+
+# Register vendor fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/euclid/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/euclid/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml