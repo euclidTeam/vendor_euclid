TenX-UDC/vendor_tenx

vendor: Define partition order for RROs
Give vendor and odm higher priority over product as we ship common
overlays in product and device-specific ones in vendor/odm. They're
also placed higher than oem as we don't build the oem partition.

Change-Id: Ie8b6e79dcaf1fa1e896ee836a56e112f26023f01
Signed-off-by: Ghosuto <clash.raja10@gmail.com>
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

config/common.mk
@@ -127,6 +127,10 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 SYSTEM_OPTIMIZE_JAVA ?= true
 SYSTEMUI_OPTIMIZE_JAVA ?= true
 
+# Partition overlay
+PRODUCT_COPY_FILES += \
+    vendor/lineage/overlay/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
+
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 

euclidTeam/vendor_euclid

config/common.mk
@@ -164,6 +164,10 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 SYSTEM_OPTIMIZE_JAVA ?= true
 SYSTEMUI_OPTIMIZE_JAVA ?= true
 
+# Partition overlay
+PRODUCT_COPY_FILES += \
+    vendor/euclid/overlay/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
+
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 