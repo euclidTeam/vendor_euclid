DroidX-UI/vendor_droidx

Build: Add emulator Configuration

build/target/product/AndroidProducts.mk
@@ -1,0 +1,21 @@
+#
+# Copyright (C) 2018-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/droidx_sdk_phone_x86_64.mk
+
+COMMON_LUNCH_CHOICES := \
+    droidx_sdk_phone_x86_64-userdebug

euclidTeam/vendor_euclid

build/target/product/AndroidProducts.mk
@@ -1,0 +1,21 @@
+#
+# Copyright (C) 2018-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/euclid_sdk_phone_x86_64.mk
+
+COMMON_LUNCH_CHOICES := \
+    euclid_sdk_phone_x86_64-userdebug