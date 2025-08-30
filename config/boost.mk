876f49de54138d8b7160d098d2dcc0d82d783476
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-30 11:19:27 +0800

    [PERF] separate bg and sys-bg cpusets
    system-bg processes are called within visible processes. e.g loading playstore components.
    
    Change-Id: Ia4562bec975e8931a81f02aa2aa6f5f7460a8249
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/boost.mk
+++ config/boost.mk
@@ -1,7 +1,8 @@
 EUCLID_CPU_SMALL_CORES ?= 0,1,2,3
 EUCLID_CPU_BIG_CORES ?= 4,5,6,7
 EUCLID_ALL_CORES ?= 0-7
-EUCLID_CPU_BG ?= 0-3
+EUCLID_CPU_SYS_BG ?= 0-3
+EUCLID_CPU_BG ?= 0-2
 EUCLID_CPU_FG ?= 0-5
 EUCLID_CPU_LIMIT_BG ?= 0-1
 EUCLID_CPU_LIMIT_UI ?= 0-2
@@ -12,6 +13,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.euclid_cpu_big=$(EUCLID_CPU_BIG_CORES) \
     persist.sys.euclid_cpu_small=$(EUCLID_CPU_SMALL_CORES) \
     persist.sys.euclid_cpu_bg=$(EUCLID_CPU_BG) \
+    persist.sys.euclid_cpu_sys_bg=$(EUCLID_CPU_SYS_BG) \
     persist.sys.euclid_cpu_limit_bg=$(EUCLID_CPU_LIMIT_BG) \
     persist.sys.euclid_cpu_fg=$(EUCLID_CPU_FG) \
     persist.sys.euclid_cpu_limit_ui=$(EUCLID_CPU_LIMIT_UI) \
