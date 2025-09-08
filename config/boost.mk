023d034aec8f72e448a207665f9c1c32f16cca23
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-08 11:57:08 +0800

    reduce sf boost
    Change-Id: Ic5a06258de3e4aae096677c355256bd8c3e1c45d
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/boost.mk
+++ config/boost.mk
@@ -24,5 +24,5 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.euclid_cpu_display=$(EUCLID_CPU_DISPLAY)
 
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.uclamp.min=100
+    ro.surface_flinger.uclamp.min=10
     persist.sys.activity_anim_perf_override=$(PERF_ANIM_OVERRIDE)
