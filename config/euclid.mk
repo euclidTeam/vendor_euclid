f49654d7976e90679a8715021da86b36dcbc38dd
techyminati <sinha.aryan03@gmail.com>
2025-09-26 09:33:11 +0000

    config: Enable Material Design 3 Expressive Change-Id: Ib6d3ac2e0f3d76768c508e1b00efc51e141c1b09 Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- config/euclid.mk
+++ config/euclid.mk
@@ -9,3 +9,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 endif
 
 PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += \
+    is_expressive_design_enabled=true
