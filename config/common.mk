GenesisOS/vendor_genesis

vendor: Add feature xml for call recording
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

config/common.mk
@@ -178,6 +178,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/genesis/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync

euclidTeam/vendor_euclid

config/common.mk
@@ -157,6 +157,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/euclid/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/euclid/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync