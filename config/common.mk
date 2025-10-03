The-Clover-Project/vendor_clover

vendor: config: Copy Lineage health permissions Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd

config/common.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/clover/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Lineage Health
+PRODUCT_COPY_FILES += \
+    vendor/clover/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync

euclidTeam/vendor_euclid

config/common.mk
@@ -161,6 +161,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/euclid/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Lineage Health
+PRODUCT_COPY_FILES += \
+    vendor/euclid/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync