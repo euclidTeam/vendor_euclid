98b38f24cace6927490a6d14def53bb5ff0fdf9b
Ghosuto <clash.raja10@gmail.com>
2025-10-18 20:03:45 +0530

    config: Fixup blur flag

--- config/euclid.mk
+++ config/euclid.mk
@@ -2,10 +2,14 @@
 TARGET_ENABLE_BLUR ?= true
 ifeq ($(TARGET_ENABLE_BLUR),true)
 PRODUCT_SYSTEM_PROPERTIES += \
-    ro.custom.blur.enable=true
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
 else
 PRODUCT_SYSTEM_PROPERTIES += \
-    ro.custom.blur.enable=false
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
 endif
 
 PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
