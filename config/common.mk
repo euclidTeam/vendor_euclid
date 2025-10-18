VoltageOS-staging/vendor_voltage

vendor: boost framework configs
Signed-off-by: Dmitrii <bankersenator@gmail.com>

config/common.mk
@@ -266,6 +266,9 @@ include vendor/voltage/config/version.mk
 # BootAnimation
 include vendor/voltage/config/bootanimation.mk
 
+# boost framework
+include vendor/voltage/config/boost.mk
+
 # Fonts
 $(call inherit-product, vendor/voltage/fonts/fonts.mk)
 

euclidTeam/vendor_euclid

Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

config/common.mk
@@ -261,6 +261,8 @@ include vendor/euclid/config/euclid.mk
 include vendor/euclid/PlayIntegrity/pif.mk
 -include vendor/euclid-priv/keys/keys.mk
 
+# boost framework
+include vendor/euclid/config/boost.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 