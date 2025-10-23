879b0acf3afad16094a1786b222fad18bd8efa6f
Mrick343 <mrinmoy8429de@gmail.com>
2025-10-23 08:51:08 +0530

    vendor: Build datura

--- config/packages.mk
+++ config/packages.mk
@@ -3,7 +3,8 @@ PRODUCT_PACKAGES += \
     Etar \
     euclidOSSetupWizard \
     ExactCalculator \
-    Glimpse 
+    Glimpse \
+    Datura 
 
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
